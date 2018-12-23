b4ee55f09c2829b2dc7eb9e6d9469b080aa9d091
Fabrizio Pairone <fabrizio.pairons@gmail.com>
2018-12-23 14:05:01 +0100

    Add JsonProperty to OPGetTask.cs

--- ClickUpAPI.V1/OptionalParams/OPGetTasks.cs
+++ ClickUpAPI.V1/OptionalParams/OPGetTasks.cs
@@ -1,4 +1,5 @@
-﻿using PaironsTech.ClickUpAPI.V1.Enums;
+﻿using Newtonsoft.Json;
+using PaironsTech.ClickUpAPI.V1.Enums;
 using System;
 using System.Collections.Generic;
 
@@ -14,81 +15,97 @@ namespace PaironsTech.ClickUpAPI.V1.OptionalParams
         /// <summary>
         /// Rappresent the Page Filter
         /// </summary>
+        [JsonProperty("page")]
         public int? Page { get; set; }
 
         /// <summary>
         /// Rappresent the order of Data
         /// </summary>
+        [JsonProperty("order_by")]
         public TaskOrderBy? OrderBy { get; set; }
 
         /// <summary>
         /// Reverse Order
         /// </summary>
+        [JsonProperty("reverse")]
         public bool? Reverse { get; set; }
 
         /// <summary>
         /// Include Subtasks
         /// </summary>
+        [JsonProperty("subtasks")]
         public bool? Subtasks { get; set; }
 
         /// <summary>
         /// Query only on spaces with this ids
         /// </summary>
+        [JsonProperty("space_ids")]
         public List<string> SpaceIds { get; set; }
 
         /// <summary>
         /// Query only on projects with this ids
         /// </summary>
+        [JsonProperty("projects_ids")]
         public List<string> ProjectIds { get; set; }
 
         /// <summary>
         /// Query only on lists with this ids
         /// </summary>
+        [JsonProperty("list_ids")]
         public List<string> ListIds { get; set; }
 
         /// <summary>
         /// Query only on status with this status
         /// </summary>
+        [JsonProperty("statuses")]
         public List<string> Statuses { get; set; }
-        
+
         /// <summary>
         /// Include Closed Task [Not set Statuses filter!]
         /// </summary>
+        [JsonProperty("include_closed")]
         public bool? IncludeClosed { get; set; }
 
         /// <summary>
         /// Query only on task assign at users with this ids
         /// </summary>
+        [JsonProperty("assignees")]
         public List<long> Assignees { get; set; }
 
         /// <summary>
         /// Filter due date greater than posix time
         /// </summary>
+        [JsonProperty("due_date_gt")]
         public DateTime? DueDateGt { get; set; }
 
         /// <summary>
         /// Filter due date less than posix time
         /// </summary>
+        [JsonProperty("due_date_lt")]
         public DateTime? DueDateLt { get; set; }
 
         /// <summary>
         /// Filter date created greater than posix time
         /// </summary>
+        [JsonProperty("date_created_gt")]
         public DateTime? DateCreatedGt { get; set; }
 
         /// <summary>
         /// Filter date created less than posix time
         /// </summary>
+        [JsonProperty("date_created_lt")]
         public DateTime? DateCreatedLt { get; set; }
 
         /// <summary>
         /// Filter date updated greater than posix time
         /// </summary>
+        [JsonProperty("date_updated_gt")]
         public DateTime? DateUpdatedGt { get; set; }
 
         /// <summary>
         /// Filter date updated less than posix time
         /// </summary>
+        [JsonProperty("date_updated_lt")]
         public DateTime? DateUpdatedLt { get; set; }
         
 
